5bd8016bf69b954d44556253dc4b3c7c69f2bb22
Alfred Lewis <alewis1719@gmail.com>
2020-10-10 21:59:29 -0500

    [i-10] Add Button component

---
+++ apps/www/src/components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button as MuiButton, CircularProgress } from "@material-ui/core";
+import { ButtonProps as MuiButtonProps } from "@material-ui/core/Button";
+
+import { useStyles } from "./style";
+
+interface Props extends MuiButtonProps {
+  pending?: boolean;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<Props> = ({ pending, children, ...props }) => {
+  const progressWidth = 20;
+  const classes = useStyles({ progressWidth, pending });
+
+  return (
+    <MuiButton {...props} disabled={pending}>
+      <div className={classes.content}>
+        <div className={classes.progress}>
+          {pending && <CircularProgress size={progressWidth} color="inherit" />}
+        </div>
+        <div>{children}</div>
+      </div>
+    </MuiButton>
+  );
+};
+
+export default Button;
