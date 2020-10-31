95da2f1740bb68a10935cf8d2cba55d83a2f7ab5
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2020-10-31 17:30:53 +0100

    [i-3] :sparkles: Add query & mutation types

--- packages/graphql/src/index.d.ts
+++ packages/graphql/src/index.d.ts
@@ -3,7 +3,17 @@ declare module '@expense-tracker/graphql' {
   export const GET_ME: DocumentNode;
   export const GET_USER: DocumentNode;
   export const GET_USERS: DocumentNode;
+  export const GET_CATEGORY: DocumentNode;
+  export const GET_CATEGORIES: DocumentNode;
+  export const GET_EXPENSE: DocumentNode;
+  export const GET_EXPENSES: DocumentNode;
 
   export const LOGIN: DocumentNode;
   export const REGISTER: DocumentNode;
+  export const ADD_CATEGORY: DocumentNode;
+  export const UPDATE_CATEGORY: DocumentNode;
+  export const REMOVE_CATEGORY: DocumentNode;
+  export const ADD_EXPENSE: DocumentNode;
+  export const UPDATE_EXPENSE: DocumentNode;
+  export const REMOVE_EXPENSE: DocumentNode;
 }
