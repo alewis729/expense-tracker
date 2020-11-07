11e3af45576acc6d2f9d86c94b8592a909bfc525
Alfred Lewis <alewis1719@gmail.com>
2020-11-07 10:43:25 -0500

    :recycle: Improve gql queries & mutations export structure

---
+++ packages/graphql/src/mutations/expenseMutations.js
@@ -0,0 +1,45 @@
+import gql from "graphql-tag";
+
+import { categoryFields, expenseFields } from "../fragments";
+
+export const ADD_EXPENSE = gql`
+  mutation ADD_EXPENSE(
+    $addExpenseInput: AddExpenseInput!
+    $withCategory: Boolean = false
+  ) {
+    addExpense(input: $addExpenseInput) {
+      ...expenseFields
+      category @include(if: $withCategory) {
+        ...categoryFields
+      }
+    }
+  }
+  ${expenseFields}
+  ${categoryFields}
+`;
+
+export const UPDATE_EXPENSE = gql`
+  mutation UPDATE_EXPENSE(
+    $id: ID!
+    $updateExpenseInput: UpdateExpenseInput!
+    $withCategory: Boolean = false
+  ) {
+    updateExpense(id: $id, input: $updateExpenseInput) {
+      ...expenseFields
+      category @include(if: $withCategory) {
+        ...categoryFields
+      }
+    }
+  }
+  ${expenseFields}
+  ${categoryFields}
+`;
+
+export const REMOVE_EXPENSE = gql`
+  mutation REMOVE_EXPENSE($id: ID!) {
+    removeExpense(id: $id) {
+      ...expenseFields
+    }
+  }
+  ${expenseFields}
+`;
