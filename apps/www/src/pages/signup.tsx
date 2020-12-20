cbec96e7d8c51adbd447945bf3d9d13881cf8d54
Alfred Lewis <alewis1719@gmail.com>
2020-12-20 12:00:36 -0500

    [i-28] :sparkles: Implement Google Authentication

--- apps/www/src/pages/signup.tsx
+++ apps/www/src/pages/signup.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useMutation } from "@apollo/client";
-import { REGISTER } from "@expense-tracker/graphql";
+import { REGISTER, AUTH_GOOGLE } from "@expense-tracker/graphql";
+import { useGoogleLogin, GoogleLoginResponse } from "react-google-login";
 import { useSnackbar } from "notistack";
 import { Box, Typography } from "@material-ui/core";
 
 import { isLoggedInVar } from "@/lib/graphql/cache";
 import { withoutAuth } from "@/hocs";
 import { AuthLayout } from "@/layouts";
-import { SignupForm, Link } from "@/components";
+import { SignupForm, Link, GoogleAuthButton } from "@/components";
 
 interface AddUserInput {
   name: string;
@@ -29,6 +30,20 @@ const SignupPage: React.FC = () => {
     },
     onError: error => enqueueSnackbar(error.message, { variant: "error" }),
   });
+  const [googleAuth, { loading: loadingGoogle }] = useMutation(AUTH_GOOGLE, {
+    onCompleted: ({ authGoogle }) => {
+      localStorage.setItem("token", authGoogle.token);
+      localStorage.setItem("userId", authGoogle.me.id);
+      isLoggedInVar(true);
+    },
+    onError: error => enqueueSnackbar(error.message, { variant: "error" }),
+  });
+  const { signIn: googleSignIn } = useGoogleLogin({
+    clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+    onSuccess: ({ accessToken }: GoogleLoginResponse) => {
+      googleAuth({ variables: { googleAuthInput: { accessToken } } });
+    },
+  });
 
   const handleSubmit = ({ name, email, password }: AddUserInput) =>
     register({
@@ -48,6 +63,12 @@ const SignupPage: React.FC = () => {
         <>
           <SignupForm pending={loading} onSubmit={handleSubmit} />
           <Box mt={2}>
+            <Box mb={2}>
+              <GoogleAuthButton
+                onClick={googleSignIn}
+                pending={loadingGoogle}
+              />
+            </Box>
             <Typography variant="body2">
               Already a member? <Link href="/signin">Sign in</Link>
             </Typography>
