af830018c09c84f66e08a97c7e8f35c6b6a71f87
Alfred Lewis <alewis1719@gmail.com>
2021-02-28 12:14:48 -0500

    Update page index -> home & arg incomePerYear -> incomesPerYear

---
+++ apps/www/src/pages/home.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_CHART_DATA } from "@expense-tracker/graphql";
+import { useSnackbar } from "notistack";
+import { Box } from "@material-ui/core";
+
+import { withAuth } from "@/hocs";
+import { DefaultLayout } from "@/layouts";
+import { Header } from "@/containers";
+import { Chart1 } from "@/containers/Charts";
+
+const Home: React.FC = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const { data, loading, error } = useQuery(GET_CHART_DATA, {
+    variables: { expensesPerYear: true, incomesPerYear: true },
+    onError: error => {
+      console.error(error);
+      enqueueSnackbar(error.message, { variant: "error" });
+    },
+  });
+
+  if (error) return <p>Something went wrong... Try refreshing the page</p>;
+
+  return (
+    <DefaultLayout header={<Header />} loading={loading} hideWhileLoading>
+      <Box mb={8} id="chart-1">
+        <Chart1
+          expensesPerYear={data?.chartData?.expensesPerYear}
+          incomesPerYear={data?.chartData?.incomesPerYear}
+          timeline={data?.chartData?.timeline}
+        />
+      </Box>
+    </DefaultLayout>
+  );
+};
+
+export default withAuth(Home);
--- packages/graphql/src/queries/chartQueries.js
+++ packages/graphql/src/queries/chartQueries.js
@@ -3,21 +3,19 @@ import gql from "graphql-tag";
 import { chartDataFields, paymentsPerYearFields } from "../fragments";
 
 // expensesPerMonth: Boolean
-// expensesPerYear: Boolean
 // incomePerMonth: Boolean
-// incomePerYear: Boolean
 
 export const GET_CHART_DATA = gql`
   query GET_CHART_DATA(
     $expensesPerYear: Boolean = false
-    $incomePerYear: Boolean = false
+    $incomesPerYear: Boolean = false
   ) {
     chartData {
       ...chartDataFields
       expensesPerYear @include(if: $expensesPerYear) {
         ...paymentsPerYearFields
       }
-      incomesPerYear @include(if: $incomePerYear) {
+      incomesPerYear @include(if: $incomesPerYear) {
         ...paymentsPerYearFields
       }
     }
